765b780fd02a41bc4d28d750e7fba3ea2d991a57
KacperZurawik <kaacper181@gmail.com>
2024-11-08 19:23:25 +0100

    Created Issue model

---
+++ LibraryConsoleApp/Models/Issue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsoleApp.Models
+{
+    internal class Issue
+    {
+        public int IssueId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+        public Issue(int issueId, string title, string name)
+        {
+            IssueId = issueId;
+            Title = title;
+            Name = name;
+        }
+    }
+}
